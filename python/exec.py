f637e324af16519f247cc3d324260002c85f2517
Moran Assif <moranassif@gmail.com>
2013-09-14 10:22:32 +0000

    Adding exec.py and buttons to motion.py

---
+++ python/exec.py
@@ -0,0 +1,11 @@
+from zoomba_controller.motion import motion
+import time
+
+m = motion()
+
+m.do_nothing()
+#m.advance("forward", 1)
+#m.advance("backward", 1)
+
+#m.turn("left", 20)
+#m.turn("right", 20)
